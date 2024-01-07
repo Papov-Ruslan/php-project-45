3d24010ea4c49d82bc8b4118a5334a2bb3a30c99
Papov-Ruslan <papovruslan7@gmail.com>
2024-01-07 17:05:26 +0300

    Игры разума

--- src/Engine.php
+++ src/Engine.php
@@ -5,46 +5,25 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function StartGame(array $finalAssocArray, string $name, string $task)
+function StartGame(string $gameName)
 {
-    line($task);
-    foreach ($finalAssocArray as $key => $value) {
-        $answer = prompt("Question: $key\nYour answer");
-        if ($answer != $value) {
-            line("'$answer' is wrong answer ;(. Correct answer was '$value'. \nLet's try again, $name!");
-            return;
+    line("Welcome to the Brain Games!");
+    $name = prompt("May I have your name?");
+    line("Hello $name");
+    $correctAnswersCount = 0;
+    $roundsCount = 3;
+    require_once(__DIR__ . "/../src/Game/" . $gameName . ".php");
+    while ($correctAnswersCount < $roundsCount) {
+        [$userAnswer, $correctAnswer] = eval('return BrainGames\Cli\brain_' . $gameName . '();');
+
+        if ($userAnswer == $correctAnswer) {
+            \cli\line("Correct!");
+            $correctAnswersCount++;
         } else {
-            line("Correct!");
+            \cli\line("'$userAnswer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+            \cli\line("Let's try again, $name!");
+            return;
         }
     }
-    line("Congratulations, $name!");
+    line("Congratulations!!!!, $name");
 }
-
-//
-//namespace BrainGames\Cli;
-//
-//use function cli\line;
-//use function cli\prompt;
-//
-//function StartGame(string $gameName)
-//{
-//    line("Welcome to the Brain Games!");
-//    $name = prompt("May I have your name?");
-//    line("Hello $name");
-//    $correctAnswersCount = 0;
-//    $roundsCount = 3;
-//    require_once(__DIR__ . "/../src/Game/" . $gameName . ".php");
-//    while ($correctAnswersCount < $roundsCount) {
-//        [$userAnswer, $correctAnswer] = eval('return BrainGames\Cli\brain_' . $gameName . '();');
-//
-//        if ($userAnswer == $correctAnswer) {
-//            \cli\line("Correct!");
-//            $correctAnswersCount++;
-//        } else {
-//            \cli\line("'$userAnswer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
-//            \cli\line("Let's try again, $name!");
-//            return;
-//        }
-//    }
-//    line("Congratulations!!!!, $name");
-//}

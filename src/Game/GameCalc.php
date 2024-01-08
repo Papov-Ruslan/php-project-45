1a63721bfc9d6405309d0635f657eaddb6af2cfa
Papov-Ruslan <papovruslan7@gmail.com>
2024-01-08 12:39:29 +0300

    Игры разума

--- src/Game/GameCalc.php
+++ src/Game/GameCalc.php
@@ -11,7 +11,7 @@ function brain_GameCalc()
     $num2 = random_int(1, 100);
     $operands = ["+", "-", "*"];
     $operand = $operands[random_int(0, 2)];
-    line("Question: " . $num1 . $operand . $num2);
+    line("Question: " . $num1 . " " . $operand . " " . $num2);
     $userAnswer = prompt("your answer:");
     $correctAnswer = eval("return $num1 $operand $num2;");
     return [$userAnswer, $correctAnswer];
